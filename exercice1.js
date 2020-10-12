a6417d3471a30f23127915f954831d6702b5af03
yvan <yvantamen@yahoo.com>
2020-10-12 21:16:14 +0200

    exercice 1

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,33 @@
 function chuckNorris(msg) {
   return "";
+  let binaire =tobinairy(msg)
+  console.log(binaire);
+  let precedent ="";
+
+  for(let i=0; i<binaire.lengh; i++){
+    if(precedent !==binaire.charAt(i)){
+
+      if(i!==0)
+      result += " ";
+      if (binaire.charAt(i)==="0"){
+        result +="00 0"
+      } else{
+        result +="0 0"
+      }
+    }else{
+      result= result + "0";
+    }
+    precedent=binaire.charAt(i);
+  }
+  return result;
+}
+
+function tobinary(string) {
+  let result = "";
+  for (let index = 0; index < string.length; index++) {
+    const element = string.charCodeAt(index).toString(2);
+    result += element;
+  }
+
+  return result;
 }
